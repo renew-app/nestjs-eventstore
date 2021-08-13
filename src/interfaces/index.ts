d800aad84d0632b7eacc8d772cba9ad8d077b9f0
Nathan Gudritz <nathan@nathans-mbp.lan>
2021-08-13 10:24:42 -0400

    Fix build

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,4 @@
 export * from './event-store-options.interface';
-export * from './event-store-feature-options.interface';
 export * from './event-store-async-options.interface';
 export * from './aggregate-event.interface';
 export * from './event-constructors.interface';
